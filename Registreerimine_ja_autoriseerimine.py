5d15f0dac21dbc029da4db7124e9c2d8755493af
Loginov10 <120027474+Loginov10@users.noreply.github.com>
2023-02-17 13:22:22 +0200

    Update Registreerimine_ja_autoriseerimine.py

--- Registreerimine_ja_autoriseerimine.py
+++ Registreerimine_ja_autoriseerimine.py
@@ -2,7 +2,7 @@ from module import*
 user=["gena","valja"]
 password=["12345","gaga"]
 while True:
-    print("1-Регистрация, 2-Авторизация, 3-Выход")
+    print("1-Регистрация, 2-Авторизация, 3-Выход, 4-Смена пароля")
     a=int(input())
     if a==1:
         user,password=registr(user,password)
@@ -12,7 +12,15 @@ while True:
 
     elif a==3:
         print("Выход")
-        #
+        break
+    
+    elif a==4:
+        print("Смена пароля")
+        izmena(user,password)
+      
+    else:
+        print("Введи правильно")
+
    
     
     
